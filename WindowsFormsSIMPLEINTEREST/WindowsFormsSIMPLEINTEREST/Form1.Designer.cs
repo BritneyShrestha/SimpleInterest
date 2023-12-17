23d91b841f6f3532748cd51dafc5e0ee67f5df40
BritneyShrestha <87053116+BritneyShrestha@users.noreply.github.com>
2023-12-17 20:44:55 +0545

    Add files via upload

---
+++ WindowsFormsSIMPLEINTEREST/WindowsFormsSIMPLEINTEREST/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsSIMPLEINTEREST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.textRate = new System.Windows.Forms.TextBox();
+            this.textTime = new System.Windows.Forms.TextBox();
+            this.textPrincipal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textResult
+            // 
+            this.textResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResult.Location = new System.Drawing.Point(385, 319);
+            this.textResult.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(221, 38);
+            this.textResult.TabIndex = 15;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(195, 322);
+            this.btnCalculate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(143, 34);
+            this.btnCalculate.TabIndex = 14;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // textRate
+            // 
+            this.textRate.Location = new System.Drawing.Point(385, 246);
+            this.textRate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textRate.Multiline = true;
+            this.textRate.Name = "textRate";
+            this.textRate.Size = new System.Drawing.Size(221, 38);
+            this.textRate.TabIndex = 13;
+            // 
+            // textTime
+            // 
+            this.textTime.Location = new System.Drawing.Point(385, 172);
+            this.textTime.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textTime.Multiline = true;
+            this.textTime.Name = "textTime";
+            this.textTime.Size = new System.Drawing.Size(221, 34);
+            this.textTime.TabIndex = 12;
+            // 
+            // textPrincipal
+            // 
+            this.textPrincipal.Location = new System.Drawing.Point(385, 93);
+            this.textPrincipal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textPrincipal.Multiline = true;
+            this.textPrincipal.Name = "textPrincipal";
+            this.textPrincipal.Size = new System.Drawing.Size(221, 32);
+            this.textPrincipal.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(227, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Rate:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(227, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Time:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Principal:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.textRate);
+            this.Controls.Add(this.textTime);
+            this.Controls.Add(this.textPrincipal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox textRate;
+        private System.Windows.Forms.TextBox textTime;
+        private System.Windows.Forms.TextBox textPrincipal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsSIMPLEINTEREST/WindowsFormsSIMPLEINTEREST/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSIMPLEINTEREST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textPrincipal.Text, out double principal) &&
+                double.TryParse(textRate.Text, out double rate) &&
+                double.TryParse(textTime.Text, out double time))
+            {
+                double si = (principal * rate * time) / 100;
+                textResult.Text = $" Simple Interest = {si}";
+            }
+            else
+            {
+                MessageBox.Show(" PLEASE ENTER VALID NUMERIC VALUES ");
+            }
+        }
+    }
+}
